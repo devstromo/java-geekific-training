38caf3c85fbf926133c605f1a102277f0ecde9ad
alemoles <alemolerio@gmail.com>
2022-09-06 00:02:08 -0300

    Tim Sorting
    Link - https://www.youtube.com/watch?v=GhP5WbE4GYo

---
+++ sorting-algorithms/src/main/java/com/devtstromo/InsertionSort.java
@@ -0,0 +1,16 @@
+package com.devtstromo;
+
+public record InsertionSort<T extends Comparable<T>>(T[] arr) {
+    public void sort(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            int j = i;
+            // change comparator sign to descending ordering
+            while (j > 0 && arr[j].compareTo(arr[j - 1]) < 0) {
+                T temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+}
