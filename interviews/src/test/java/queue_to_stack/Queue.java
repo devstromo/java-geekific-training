7e73b00b1fd1a081fac97c5fee49841d49a5667c
alemoles <alemolerio@gmail.com>
2022-12-25 22:18:27 -0300

    Implement Queue using Stacks

---
+++ interviews/src/test/java/queue_to_stack/Queue.java
@@ -0,0 +1,37 @@
+package queue_to_stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Queue<T> {
+    private final Deque<T> stack1 = new LinkedList<>();
+    private final Deque<T> stack2 = new LinkedList<>();
+
+    public void enqueue(T value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public T dequeue() {
+        if (stack1.isEmpty()) {
+            return null;
+        }
+        return stack1.pop();
+    }
+
+    public T peek() {
+        if (stack1.isEmpty()) {
+            return null;
+        }
+        return stack1.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+}
---
+++ interviews/src/test/java/queue_to_stack/QueueTest.java
@@ -0,0 +1,19 @@
+package queue_to_stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QueueTest {
+
+    @Test
+    public void testQueue(){
+        Queue<Integer> myQueue = new Queue<>();
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        assertEquals(1, myQueue.peek());
+        assertEquals(1, myQueue.dequeue());
+    }
+
+}
