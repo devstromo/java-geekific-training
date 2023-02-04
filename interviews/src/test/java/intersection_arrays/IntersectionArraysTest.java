f64b8cffaa59ca0927936160ce6f015d9b3cef2a
alemoles <alemolerio@gmail.com>
2023-02-04 20:45:00 -0300

    Intersection of Arrays | Sorted Array with Binary Search

--- interviews/src/test/java/intersection_arrays/IntersectionArraysTest.java
+++ interviews/src/test/java/intersection_arrays/IntersectionArraysTest.java
@@ -35,4 +35,13 @@ class IntersectionArraysTest {
         assertArrayEquals(result, IntersectionArrays.intersectionWithSortedArrays(nums1, nums2));
     }
 
+    @Test
+    public void testIntersectionWithBinarySearch() {
+        var result = new int[] { 2, 3 };
+        var nums1 = new int[] { 100, 1, 2, 2, 3, 3, 4, 4, 5, 6 };
+        var nums2 = new int[] { 10, 11, 19, 18, 16, 13, 57, 2, 54, 78, 65, 3 };
+        sort(nums1);
+        assertArrayEquals(result, IntersectionArrays.intersectionWithSortedArraysAndBinarySearch(nums1, nums2));
+    }
+
 }
