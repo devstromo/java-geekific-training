d8f5e5e5531ac35dfca3241effea7665852d87a7
alemoles <alemolerio@gmail.com>
2023-02-18 18:05:46 -0300

    Two sum problem

--- interviews/src/test/java/two_sum/TwoSumTest.java
+++ interviews/src/test/java/two_sum/TwoSumTest.java
@@ -9,15 +9,29 @@ class TwoSumTest {
     @Test
     public void testTwoSum() {
         var input = new int[] { 2, 8, 4, 6, 1, 78, 12, 45, 19 };
-        var expected = new int[] {1, 78};
+        var expected = new int[] { 1, 78 };
         assertArrayEquals(expected, TwoSum.twoSum(input, 79));
     }
 
     @Test
     public void testTwoSumIndex() {
         var input = new int[] { 2, 8, 4, 6, 1, 78, 12, 45, 19 };
-        var expected = new int[] {4, 5};
+        var expected = new int[] { 4, 5 };
         assertArrayEquals(expected, TwoSum.twoSumIndexes(input, 79));
+    }
+
+    @Test
+    public void testTwoSumNotFound() {
+        var input = new int[] { 2, 8, 4, 6, 1, 78, 12, 45, 19 };
+        var expected = new int[] { 0, 0 };
+        assertArrayEquals(expected, TwoSum.twoSum(input, 145));
+    }
+
+    @Test
+    public void testTwoSumIndexNotFound() {
+        var input = new int[] { 2, 8, 4, 6, 1, 78, 12, 45, 19 };
+        var expected = new int[] { 0, 0 };
+        assertArrayEquals(expected, TwoSum.twoSumIndexes(input, 145));
     }
 
 }
