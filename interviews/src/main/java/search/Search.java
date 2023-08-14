8fb1ef717a2b5b019b52390402570990dee2bfc0
alemoles <alemolerio@gmail.com>
2023-08-14 20:08:52 -0300

    Search In Rotated Sorted Array

---
+++ interviews/src/main/java/search/Search.java
@@ -0,0 +1,34 @@
+package search;
+
+public class Search {
+
+    /**
+     * Search in rotate array
+     */
+    public static int searchInRotate(int[] nums, int target) {
+        var left = 0;
+        var right = nums.length - 1;
+        while (left <= right) {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            // the first subarray is sorted
+            if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            // the second subarray is sorted
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
