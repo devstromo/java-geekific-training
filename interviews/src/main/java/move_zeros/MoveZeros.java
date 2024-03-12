82614454d41164a4bedbdaa382838bd76164ee92
alemoles <alemolerio@gmail.com>
2024-03-11 21:48:33 -0300

    Move Zeroes | Brute Force O(n^2)

---
+++ interviews/src/main/java/move_zeros/MoveZeros.java
@@ -0,0 +1,19 @@
+package move_zeros;
+
+public class MoveZeros {
+
+    // O(n^2)
+    public static int[] moveZeros(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] == 0) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+
+        }
+        return nums;
+    }
+}
---
+++ interviews/src/test/java/move_zeros/MoveZerosUnitTest.java
@@ -0,0 +1,16 @@
+package move_zeros;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveZerosUnitTest {
+    private final MoveZeros moveZeros = new MoveZeros();
+
+    @Test
+    public void testMoveZero() {
+        var input = new int[]{3, 0, 6, 7, 8, 9, 0, 0, 2, 0};
+        var expected = new int[]{3, 6, 7, 8, 9, 2, 0, 0, 0, 0};
+        assertArrayEquals(expected, MoveZeros.moveZeros(input));
+    }
+}
