dcc8a66928aa0a0f5ea0225623f851530ee1d4fe
alemoles <alemolerio@gmail.com>
2024-03-11 21:55:46 -0300

    Move Zeroes | Linear Time

--- interviews/src/main/java/move_zeros/MoveZeros.java
+++ interviews/src/main/java/move_zeros/MoveZeros.java
@@ -1,8 +1,11 @@
 package move_zeros;
 
+import java.util.Arrays;
+
 public class MoveZeros {
 
-    // O(n^2)
+    // T: O(n^2)
+    // S: O(1)
     public static int[] moveZerosBruteForce(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length - 1; j++) {
@@ -14,5 +17,20 @@ public class MoveZeros {
             }
         }
         return nums;
+    }
+
+    // T: O(n)
+    // S: O(n)
+    public static int[] moveZerosLinear(int[] nums) {
+        var arr = new int[nums.length];
+        var insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                arr[insertPos++] = num;
+            }
+        }
+
+        System.arraycopy(arr, 0, nums, 0, nums.length);
+        return arr;
     }
 }
--- interviews/src/test/java/move_zeros/MoveZerosUnitTest.java
+++ interviews/src/test/java/move_zeros/MoveZerosUnitTest.java
@@ -5,12 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveZerosUnitTest {
-    private final MoveZeros moveZeros = new MoveZeros();
 
     @Test
     public void testMoveZero() {
         var input = new int[]{3, 0, 6, 7, 8, 9, 0, 0, 2, 0};
         var expected = new int[]{3, 6, 7, 8, 9, 2, 0, 0, 0, 0};
         assertArrayEquals(expected, MoveZeros.moveZerosBruteForce(input));
+    }
+
+    @Test
+    public void testMoveZero2() {
+        var input = new int[]{3, 0, 6, 7, 8, 9, 0, 0, 2, 0};
+        var expected = new int[]{3, 6, 7, 8, 9, 2, 0, 0, 0, 0};
+        assertArrayEquals(expected, MoveZeros.moveZerosLinear(input));
     }
 }
